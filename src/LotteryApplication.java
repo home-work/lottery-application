ed08022648aa8dfa83cbf793362afd13f0c2aff8
Tien Dat <datdta95@gmail.com>
2018-08-10 15:23:55 +0700

    Nhập vào 2 chữ số của giải

--- src/LotteryApplication.java
+++ src/LotteryApplication.java
@@ -1,9 +1,18 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class LotteryApplication {
     public static void main(String[] args) {
         //Random 1 số có 2 chữ số
         Random random = new Random();
         int lottery = random.nextInt(100);
+
+        //Nhập vào 2 chữ số của giải
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập vào chữ số đầu tiên của giải: ");
+        int guessDigit1 = scanner.nextInt();
+        System.out.println("Nhập vào chữ số thứ hai của giải: ");
+        int guessDigit2 = scanner.nextInt();
+
     }
 }
